518ddb4337f8454f1c9bd6bf04a1403205d40a1b
basestylo <base.dks@gmail.com>
2013-10-05 07:33:45 +0200

    update con todo el material de la parte 2 nueva

--- SisInteligentesP1/src/infEnlace.java
+++ SisInteligentesP1/src/infEnlace.java
@@ -1,7 +1,9 @@
 import java.util.Collection;
+import java.util.Iterator;
+
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
 
-//TODO devolver tambien distancia y vias al mostrar
+
 public class infEnlace {
 
 	private long id,nodo1, nodo2;
@@ -11,11 +13,13 @@ public class infEnlace {
 		this.id=id;
 		this.nodo1=nodo1;
 		this.nodo2=nodo2;
+		this.informacion=informacion;
 	}
 	public infEnlace(long id,long nodo1,long nodo2,Collection<Tag> informacion,boolean oneway){
 		this.id=id;
 		this.nodo1=nodo1;
 		this.nodo2=nodo2;
+		this.informacion=informacion;
 		this.wrongway=oneway;
 	}
 	public long getIdNodoOrigen(){
@@ -23,5 +27,14 @@ public class infEnlace {
 	}
 	public long getIdNodoDestino(){
 		return nodo2;
+	}
+	public String getName() {
+		Tag tag;
+		Iterator <Tag> camposInfo;
+		camposInfo=informacion.iterator();
+		do{
+			tag=camposInfo.next();
+		}while(!tag.getKey().equals("name")||!camposInfo.hasNext());
+		return tag.getValue();
 	}
 }
